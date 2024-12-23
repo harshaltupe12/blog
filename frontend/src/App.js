c3193bb09d15ace205f52af5ff470449e9e45d53
Harshal Tupe <harshaltupe12@gmail.com>
2024-12-23 23:45:55 +0530

    Setup after JWT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,43 @@
-// frontend/src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import BlogList from './pages/BlogList';
-import BlogDetail from './pages/BlogDetail';
-import CreateBlog from './pages/CreateBlog';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail";
+import CreateBlog from "./pages/CreateBlog";
 
-function App() {
+// Private route to protect certain routes
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
+
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BlogList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-        <Route path="/create" element={<CreateBlog />} />
-      </Routes>
-    </Router>
+    <div className="bg-gray-100 min-h-screen">
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<BlogList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Protected Route for Blog Detail */}
+          <Route path="/blogs/:id" element={<BlogDetail />} />
+          
+          {/* Protected Route for Creating a Blog */}
+          <Route
+            path="/create"
+            element={
+              <PrivateRoute>
+                <CreateBlog />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
