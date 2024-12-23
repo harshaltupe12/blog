bd168a69d686caef276cbdbbf43cfd109b0ca92a
Harshal Tupe <harshaltupe12@gmail.com>
2024-12-23 22:37:11 +0530

    Entry point

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
